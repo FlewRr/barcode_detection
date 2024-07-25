b0a7559af00b076c7c1b2068629882989f595beb
rafiz <cool.adsdad@mail.ru>
2024-07-25 14:46:08 +0300

    .

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ rm -r build
 mkdir build
 cmake -S . -B build/
 cd build/
-make -j 16
-./main /home/rafiz/Desktop/barcode_detection/barcode_detection/Image /home/rafiz/Desktop/barcode_detection/barcode_detection/Markup
+make
+./main $IMAGES_PATH $MARKUP_PATH
