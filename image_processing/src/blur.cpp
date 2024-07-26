c13f4c30e21ec1ff44d8ec8dc7839d0cba40c6e0
FlewRr <cool.adsdad@mail.ru>
2024-07-26 17:41:17 +0300

    .

--- image_processing/src/blur.cpp
+++ image_processing/src/blur.cpp
@@ -1,26 +1,51 @@
 #include "image_processing.h"
 
 void integral_image(const cv::Mat& src, cv::Mat& dst){
-    dst = cv::Mat(src.rows, src.cols, CV_64FC3);
-    for (int y = 1; y < dst.rows; ++y){
-        for (int c = 0; c < 3; ++c){
-            dst.at<cv::Vec3d>(y, 0)[c] = dst.at<cv::Vec3d>(y-1, 0)[c] + src.at<cv::Vec3d>(y, 0)[c];
-        }
-    }
-    for (int x = 1; x < dst.cols; ++x){
-        for (int c = 0; c < 3; ++c){
-            dst.at<cv::Vec3d>(0, x)[c] = dst.at<cv::Vec3d>(0, x-1)[c] + src.at<cv::Vec3d>(0, x)[c];
-        }
-    }
-    for (int y = 1; y < dst.rows; ++y){
-        for (int x = 1; x < dst.cols; ++x){
-            for (int c = 0; c < 3; ++c){
-                dst.at<cv::Vec3d>(y, x)[c] = dst.at<cv::Vec3d>(y, x-1)[c] + dst.at<cv::Vec3d>(y-1, x)[c] - dst.at<cv::Vec3d>(y-1, x-1)[c] + src.at<cv::Vec3d>(y, x)[c];
+    // dst = cv::Mat(src.rows, src.cols, CV_64FC3);
+    // for (int y = 1; y < dst.rows; ++y){
+    //     for (int c = 0; c < 3; ++c){
+    //         dst.at<cv::Vec3d>(y, 0)[c] = dst.at<cv::Vec3d>(y-1, 0)[c] + src.at<cv::Vec3d>(y, 0)[c];
+    //     }
+    // }
+    // for (int x = 1; x < dst.cols; ++x){
+    //     for (int c = 0; c < 3; ++c){
+    //         dst.at<cv::Vec3d>(0, x)[c] = dst.at<cv::Vec3d>(0, x-1)[c] + src.at<cv::Vec3d>(0, x)[c];
+    //     }
+    // }
+    // for (int y = 1; y < dst.rows; ++y){
+    //     for (int x = 1; x < dst.cols; ++x){
+    //         for (int c = 0; c < 3; ++c){
+    //             dst.at<cv::Vec3d>(y, x)[c] = dst.at<cv::Vec3d>(y, x-1)[c] + dst.at<cv::Vec3d>(y-1, x)[c] - dst.at<cv::Vec3d>(y-1, x-1)[c] + src.at<cv::Vec3d>(y, x)[c];
+    //         }
+    //     }
+    // }
+
+    // dst.convertTo(dst, CV_32S);
+
+    cv::Mat img = src.clone();
+    img.convertTo(img, CV_64FC3);
+    dst = cv::Mat(img.rows, img.cols, CV_64FC3);
+
+    for (int row = 0; row < src.rows; ++row){
+        double* p = dst.ptr<double>(row);
+        double* q = img.ptr<double>(row);
+        double* prev;
+        for (int col = 0; col < 3 * src.cols; ++col){
+            if (col < 3 && row == 0){
+                p[col] = q[col];
+            }
+            else if (col < 3){
+                p[col] = prev[col] + q[col];
+            }
+            else if (row == 0){
+                p[col] = p[col-3] + q[col];
+            }
+            else{
+                p[col] = p[col-3] + prev[col] - prev[col-3] + q[col];
             }
         }
+        prev = dst.ptr<double>(row);
     }
-
-    dst.convertTo(dst, CV_32SC3);
 }
 
 void blur(const cv::Mat& src, cv::Mat& dst, int ksize){
@@ -34,10 +59,11 @@ void blur(const cv::Mat& src, cv::Mat& dst, int ksize){
 
     int radius = (ksize - 1) / 2;
     dst = cv::Mat::zeros(src.size(), CV_64FC3);
-    cv::Mat integral = cv::Mat(src.rows, src.cols, CV_64FC3, 0.0);
-    cv::Mat img = src.clone();
-    img.convertTo(img, CV_64FC3);
-    integral_image(img, integral);
+    cv::Mat integral;
+    //  = cv::Mat(src.rows, src.cols, CV_64FC3);
+    // cv::Mat img = src.clone();
+    // img.convertTo(img, CV_64FC3);
+    integral_image(src, integral);
 
     for (int y = radius; y < src.rows-radius; ++y) {
         for (int x = radius; x < src.cols-radius; ++x) {
@@ -66,7 +92,7 @@ void blur(const cv::Mat& src, cv::Mat& dst, int ksize){
         }
     }
 
-    cv::Rect cropped(cv::Point(0, dst.rows-ksize), cv::Point(dst.cols-ksize, 0));
+    cv::Rect cropped(cv::Point(ksize, dst.rows-ksize), cv::Point(dst.cols-ksize, ksize));
     dst = cv::Mat(dst, cropped);
     dst.convertTo(dst, CV_8UC3);
 }
--- main.cpp
+++ main.cpp
@@ -11,47 +11,57 @@
 #include "detection.h"
 
 
-int main(int argc, char* argv[]){
-    std::vector<std::string> files; 
-    std::vector<std::string> markup_files; 
-    double tp = 0, fp = 0, fn = 0, recall = 0, precision = 0, working_time = 0;
+// int main(int argc, char* argv[]){
+//     std::vector<std::string> files; 
+//     std::vector<std::string> markup_files; 
+//     double tp = 0, fp = 0, fn = 0, recall = 0, precision = 0, working_time = 0;
 
 
-    if (argc <= 2){
-        throw std::invalid_argument("Invalid parameters.");
-    }
+//     if (argc <= 2){
+//         throw std::invalid_argument("Invalid parameters.");
+//     }
 
-    std::string path(argv[1]);
-    for (const auto & entry : std::filesystem::directory_iterator(path))
-        files.push_back(entry.path());   
+//     std::string path(argv[1]);
+//     for (const auto & entry : std::filesystem::directory_iterator(path))
+//         files.push_back(entry.path());   
 
-    std::string markup_path(argv[2]);
-    for (const auto& entry: std::filesystem::directory_iterator(markup_path))
-        markup_files.push_back(entry.path());
+//     std::string markup_path(argv[2]);
+//     for (const auto& entry: std::filesystem::directory_iterator(markup_path))
+//         markup_files.push_back(entry.path());
         
-    std::sort(files.begin(), files.end());
-    std::sort(markup_files.begin(), markup_files.end());
+//     std::sort(files.begin(), files.end());
+//     std::sort(markup_files.begin(), markup_files.end());
 
 
-    auto t_start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < files.size(); ++i){
-        Detector detector;
-        auto work_start = std::chrono::high_resolution_clock::now();
-        std::vector<cv::Rect> preds = detector.detect(files[i]);
-        auto work_end = std::chrono::high_resolution_clock::now();
-        working_time += std::chrono::duration<double, std::milli>(work_end-work_start).count() * 0.001;
+//     auto t_start = std::chrono::high_resolution_clock::now();
+//     for (int i = 0; i < files.size(); ++i){
+//         Detector detector;
+//         auto work_start = std::chrono::high_resolution_clock::now();
+//         std::vector<cv::Rect> preds = detector.detect(files[i]);
+//         auto work_end = std::chrono::high_resolution_clock::now();
+//         working_time += std::chrono::duration<double, std::milli>(work_end-work_start).count() * 0.001;
 
-        std::vector<cv::Rect> ground_truth = get_markup(&markup_files[i][0]);
+//         std::vector<cv::Rect> ground_truth = get_markup(&markup_files[i][0]);
 
-        std::tuple<double, double, double> results = count_metrics(preds, ground_truth);
-        tp += std::get<0>(results);
-        fp += std::get<1>(results);
-        fn += std::get<2>(results);
+//         std::tuple<double, double, double> results = count_metrics(preds, ground_truth);
+//         tp += std::get<0>(results);
+//         fp += std::get<1>(results);
+//         fn += std::get<2>(results);
 
-    }   
-    auto t_end = std::chrono::high_resolution_clock::now();
-    recall = tp/(tp+fn);
-    precision = tp/(tp+fp);
-    std::cout << "TP: " << tp << "\n" << "FP: " << fp << "\n" << "FN: " << fn << "\n" << "Precision: " << precision << "\n" << 
-    "Recall: " << recall << "\n" << "Time elapsed: " << std::chrono::duration<double, std::milli>(t_end-t_start).count() * 0.001 << "\n" << "Average time for single image: "<< working_time / 218 << "\n";
+//     }   
+//     auto t_end = std::chrono::high_resolution_clock::now();
+//     recall = tp/(tp+fn);
+//     precision = tp/(tp+fp);
+//     std::cout << "TP: " << tp << "\n" << "FP: " << fp << "\n" << "FN: " << fn << "\n" << "Precision: " << precision << "\n" << 
+//     "Recall: " << recall << "\n" << "Time elapsed: " << std::chrono::duration<double, std::milli>(t_end-t_start).count() * 0.001 << "\n" << "Average time for single image: "<< working_time / 218 << "\n";
+// }
+
+
+int main() {
+    cv::Mat A = cv::imread("/home/sfleur/barcode_detection/barcode_detection/photo_2023-10-01_15-47-52.jpg");
+    cv::Mat B;
+
+    blur(A, B, 15);
+
+    cv::imwrite("/home/sfleur/barcode_detection/barcode_detection/1.jpg", B);
 }
